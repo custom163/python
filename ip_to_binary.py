0646895656469f2cd918ffad6fce449ba0612638
Isreal Varela <isrealv@gmail.com>
2019-05-09 17:20:14 +0000

    Create function

--- ip_to_binary.py
+++ ip_to_binary.py
@@ -1,12 +1,20 @@
-#!/usr/bin/Python
+#!/usr/bin/python
 
 ip = raw_input("Please enter the IP address: \n")
 
 list = ip.split(".")
 
-for i in list:
-    new = bin(int(i))
-    new = new.split("b")[1]
-    while len(new) < 8:
-        new = "0" + new
-    print new
+def convert(ip_list):
+    x = []
+    for i in list:
+        new = bin(int(i))
+        new = new.split("b")[1]
+        while len(new) < 8:
+            new = "0" + new
+        x.append(new)
+    return x
+
+print( ".".join(convert(list)) )
+
+
+
